527897bcdcc0d14dbdcdf930d0106164ab48a46a
keeksreid <keeks.reid@gmail.com>
2017-07-10 19:19:48 +0100

    Add files via upload
    My beauty page so far. going to add some actual text now :) x

---
+++ Beauty page/js/jquery.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.bxslider').bxSlider();
+});
+$('.bxslider').bxSlider({
+	mode: 'fade',
+	captions: true,
+	pagerCustom: '#bx-pager',
+	adaptiveHeight: true,
+	slideWidth: 300
+});
